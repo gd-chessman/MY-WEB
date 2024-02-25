29b2a6ed665d04e2a798da4fc681d139e4aef2d6
levanquy0101 <quyle.01012002@gmail.com>
2024-02-25 17:43:52 +0700

    up

--- study-project/src/App.js
+++ study-project/src/App.js
@@ -6,8 +6,8 @@ import Course from './pages/Course';
 function App() {
   return (
       <Routes>
-        <Route path="levanquy.com" element={<HomePage />} />
-        <Route path="levanquy.com/course" element={<Course />} />
+        <Route path="https://gd-chessman.github.io/MY-WEB/" element={<HomePage />} />
+        <Route path="/course" element={<Course />} />
       </Routes>
   );
 }
