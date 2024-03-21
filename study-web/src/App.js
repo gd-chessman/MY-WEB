e39b9ee0e07ea5268767c28af84a5ea254777c48
levanquy0101 <quyle.01012002@gmail.com>
2024-03-21 14:55:15 +0700

    git

--- study-web/src/App.js
+++ study-web/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/Home/Home';
 import Course from './pages/Course/Course';
 import Content from './pages/TEST/Content';
+import Login from './pages/Login/Login';
 // import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/course" element={<Course />} />
         <Route path='/content' element={<Content />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
   );
 }
